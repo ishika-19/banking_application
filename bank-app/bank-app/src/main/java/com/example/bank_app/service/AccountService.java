f73e2571528e7547a82039a05e5ea225d6f9cb64
Ishika Aggarwal <115585535+ishika-19@users.noreply.github.com>
2024-07-27 23:21:32 +0530

    Add files via upload

---
+++ bank-app/bank-app/src/main/java/com/example/bank_app/service/AccountService.java
@@ -0,0 +1,20 @@
+package com.example.bank_app.service;
+
+import com.example.bank_app.dto.AccountDto;
+
+import java.util.List;
+
+public interface AccountService {
+
+    AccountDto createAccount(AccountDto accountDto);
+
+    AccountDto getAccountById(Long id);
+
+    AccountDto deposit(Long id, double amount);
+
+    AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
+
+    void deleteAccount(Long id);
+}
